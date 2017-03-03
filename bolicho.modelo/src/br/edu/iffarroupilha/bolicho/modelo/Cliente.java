2b1bcb6a21d1247edc6a95c8ed7f17dc60e49487
João Artur Disconzi <Joo Artur Disconzi@AsusZ450L-JAD>
2017-03-02 21:57:14 -0300

    adicionando a estrutura basica do projeto bolicho.modelo, ja com as entidades: Cliente, Produto, Venda, TtemVenda

---
+++ bolicho.modelo/src/br/edu/iffarroupilha/bolicho/modelo/Cliente.java
@@ -0,0 +1,24 @@
+package br.edu.iffarroupilha.bolicho.modelo;
+
+public class Cliente {
+
+	private int cpf;
+	private String nome;
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
