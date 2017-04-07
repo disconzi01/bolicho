b81eeec974f74b670e6e535d4665dbb93ed57ca9
João Artur Disconzi <Joo Artur Disconzi@AsusZ450L-JAD>
2017-04-07 18:56:37 -0300

    Feitas as alterações conforme o professor fernando pediu.
    Hibernate na Produto

--- bolicho.modelo/src/br/edu/iffarroupilha/bolicho/modelo/Produto.java
+++ bolicho.modelo/src/br/edu/iffarroupilha/bolicho/modelo/Produto.java
@@ -1,9 +1,21 @@
 package br.edu.iffarroupilha.bolicho.modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Produto {
 
+
+	@Id // chave primaria
+	@Column(nullable=false) // campo obrigatorio no banco
 	private int idProduto;
+	
+	@Column(length=40, nullable=false)
 	private String descricao;
+	
+	@Column(nullable=false)
 	private double preco;
 
 	public int getIdProduto() {
