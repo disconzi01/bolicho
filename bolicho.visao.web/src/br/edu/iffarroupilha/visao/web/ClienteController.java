4bf1d48036b4dcb7e6286bfbd83f5da5413feb93
João Artur Disconzi <Joo Artur Disconzi@AsusZ450L-JAD>
2017-04-27 22:17:50 -0300

    Inserindo o projeto web

---
+++ bolicho.visao.web/src/br/edu/iffarroupilha/visao/web/ClienteController.java
@@ -0,0 +1,17 @@
+package br.edu.iffarroupilha.visao.web;
+
+import javax.swing.plaf.synth.SynthStyleFactory;
+
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class ClienteController {
+
+	public void listar() {
+		System.out.println("aqui vamos buscar os clientes");
+
+	}
+	
+	
+	
+}
