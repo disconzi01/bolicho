be53644f07daa63c8b2bd358ab9956bfbd3a60f6
João Artur Disconzi <Joo Artur Disconzi@AsusZ450L-JAD>
2017-05-05 21:00:15 -0300

    Adicionando a chamada gravar no banco

--- bolicho.visao.web/src/br/edu/iffarroupilha/visao/web/ClienteController.java
+++ bolicho.visao.web/src/br/edu/iffarroupilha/visao/web/ClienteController.java
@@ -1,17 +1,29 @@
 package br.edu.iffarroupilha.visao.web;
 
-import javax.swing.plaf.synth.SynthStyleFactory;
-
+import bolicho.controle.ClienteControle;
 import br.com.caelum.vraptor.Resource;
+import br.edu.iffarroupilha.bolicho.modelo.Cliente;
 
 @Resource
 public class ClienteController {
 
 	public void listar() {
 		System.out.println("aqui vamos buscar os clientes");
-
 	}
 	
-	
-	
+	public void gravar(){
+		
+		ClienteControle controle = new ClienteControle();
+		
+		// objeto temporario
+		Cliente c = new Cliente();
+		c.setNome("Jose");
+		c.setCpf(123);
+		
+		controle.gravar(c);
+	}
+
 }
+
+
+//http://localhost:8080/bolicho.visao.web/cliente/listar
